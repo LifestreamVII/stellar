a8ce9d4684dbc7a7dc6632aedfe05d353c53eac2
Loïs L <36632834+LifestreamVII@users.noreply.github.com>
2023-09-04 01:01:36 +0200

    Create background.jsx

---
+++ app/components/background.jsx
@@ -0,0 +1,12 @@
+<div class="backcomm" style="
+    position: fixed;
+    width: 100vw;
+    left: 0;
+    height: 100%;
+    z-index: 0;
+    opacity: 0.07;
+    background: url(https://images.unsplash.com/photo-1598586958772-8bf368215c2a?ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fHw%3D&amp;w=1000&amp;q=80);
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: left center;
+"></div>
