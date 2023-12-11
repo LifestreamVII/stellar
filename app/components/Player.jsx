2a16cdbaadd33a80b52c9dc521ed7acc0e1c0e76
Loïs L <36632834+LifestreamVII@users.noreply.github.com>
2023-12-11 18:58:12 +0100

    Player

--- app/components/Player.jsx
+++ app/components/Player.jsx
@@ -6,12 +6,14 @@ function Player() {
         <div class="cover-art"></div>
         <div className='player-info'>
           <div class="previous">
-              <h1 className="mb-none u__bold u__fs__normal">PREVIOUS</h1>
-              <div className='mb-m'>
-                <span>Previous Track</span>
-                <span> · </span>
-                <span>Previous Artist</span>
+            <h1 className="mb-none u__bold u__fs__normal">PREVIOUS</h1>
+            <div className='previous-info'>
+              <div class="play-pause-btn"><i class="fas fa-play"></i></div>
+              <div className='text'>
+                <p className='mb-none mt-none'>Previous Track</p>
+                <p className='mb-none mt-none u__bold'>Previous Artist</p>
               </div>
+            </div>
           </div>
           <div class="track-info">
             <div className='track-info-title'>
@@ -24,12 +26,14 @@ function Player() {
               </div>
           </div>
           <div class="next">
-              <h1 className="mb-none u__bold u__fs__normal">NEXT UP</h1>
-              <div className='mb-m'>
-                <span>Next Track</span>
-                <span> · </span>
-                <span>Next Artist</span>
+            <h1 className="mb-none u__bold u__fs__normal">NEXT UP</h1>
+            <div className='next-info'>
+              <div className='text'>
+                <p className='mb-none mt-none'>Next Track</p>
+                <p className='mb-none mt-none u__bold'>Next Artist</p>
               </div>
+              <div class="play-pause-btn"><i class="fas fa-play"></i></div>
+            </div>
           </div>
         </div>
         <div class="progress-bar-container">
